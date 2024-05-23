02706e21ed93086e48004428d561205ac671e72a
tarun12rt <104968735+tarun12rt@users.noreply.github.com>
2024-05-23 20:11:16 +0530

    working on local Appium Engine

--- src/main/java/org/example/pages/driverFactory/LocalBrowserEngine.java
+++ src/main/java/org/example/pages/driverFactory/LocalBrowserEngine.java
@@ -2,7 +2,6 @@ package org.example.pages.driverFactory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.example.pages.config.GlobalConfig;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -12,7 +11,7 @@ import org.openqa.selenium.safari.SafariDriver;
 
 public class LocalBrowserEngine {
 
-    public static RemoteWebDriver fnChromeBrowser(){
+    public static RemoteWebDriver fnChromeBrowser() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -20,29 +19,31 @@ public class LocalBrowserEngine {
         return new ChromeDriver(options);
     }
 
-    public static RemoteWebDriver fnFirefoxBrowser(){
+    public static RemoteWebDriver fnFirefoxBrowser() {
         WebDriverManager.firefoxdriver().setup();
         return new FirefoxDriver();
     }
 
-    public static RemoteWebDriver fnSafariBrowser(){
+    public static RemoteWebDriver fnSafariBrowser() {
         WebDriverManager.safaridriver().setup();
         return new SafariDriver();
     }
 
-    public static RemoteWebDriver fnEdgeBrowser(){
+    public static RemoteWebDriver fnEdgeBrowser() {
         WebDriverManager.edgedriver().setup();
         return new EdgeDriver();
     }
 
-    public static RemoteWebDriver GetLocalBrowserEngine(GlobalConfig objConfig){
-        if (objConfig.browserType==GlobalConfig.BrowserType.EDGE)
+    public static RemoteWebDriver GetLocalBrowserEngine(GlobalConfig objConfig) {
+        if (objConfig.browserType == GlobalConfig.BrowserType.EDGE)
             return fnEdgeBrowser();
-        else if (objConfig.browserType==GlobalConfig.BrowserType.FIREFOX)
-            return fnEdgeBrowser();
-         else if (objConfig.browserType==GlobalConfig.BrowserType.SAFARI)
+        else if (objConfig.browserType == GlobalConfig.BrowserType.FIREFOX)
+            return fnFirefoxBrowser();
+        else if (objConfig.browserType == GlobalConfig.BrowserType.SAFARI)
             return fnSafariBrowser();
-         else
-             return fnChromeBrowser();
+        else if (objConfig.browserType == GlobalConfig.BrowserType.CHROME)
+            return fnChromeBrowser();
+        else
+            return fnChromeBrowser();
     }
 }
